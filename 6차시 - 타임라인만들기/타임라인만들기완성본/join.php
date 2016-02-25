985f486aa66dcb4ef9353ad3f76454bacbffdc6e
Yang Gihyun <dexterastin@gmail.com>
2016-02-25 22:30:08 +0900

    4~6Class PHP Lesson file Upload.

---
+++ 6차시 - 타임라인만들기/타임라인만들기완성본/join.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>회원가입</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form method="post" action="join_ck.php">
+
+		<input name = "id" placeholder="id">
+		<input name = "title" placeholder="타이틀">
+		<input name = "password" placeholder="password">
+		<input name = "name" placeholder="이름">
+
+		<button type="submit">submit</button>
+	</form>
+</body>
+</html>
